dc21879425fc657661964f7e7182f63b20bfad52
Jackmue <72521708+Jackmue@users.noreply.github.com>
2020-12-19 16:40:32 +0100

    Firstbranch (#1) (#2)
    * Initial commit
    
    * Initial commit
    
    * Initial commit
    
    * Initial commit
    
    * Initial commit
    
    * Initial commit
    
    * Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # charger
+
 This is a code for a drone charging station.
 The drone that I use is a Dji Tello.
--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from motor_control import *
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print('Hi this is start')
+    #drone flight
+    print("drone landed")
+
+    sleighs_forward()
+    print("sleighs kissed the Drone")
+    sleighs_backward()
+    #Drone getting charged
+    print("drone ready to fly")
+
 
     print("hello_Charger_its_me")
     print("bla")
@@ -19,5 +19,4 @@ if __name__ == '__main__':
     print("blu")
     print("blx")
     print("new")
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    exit(0)
